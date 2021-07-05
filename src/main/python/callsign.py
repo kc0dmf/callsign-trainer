17a7e489b56e0a112a2342a5895452b703981778
craig larsen <craig.larsen@solutiondesign.com>
2021-07-04 22:46:36 -0500

    separates callsign generation from playing callsign

--- src/main/python/callsign.py
+++ src/main/python/callsign.py
@@ -16,6 +16,9 @@ PAUSE_SPEED = 750
 PAUSE_BETW_CALLSIGNS = 0.75
 PAUSE_BETW_LETTERS = 0.10
 
+MAX_NUM_FIRST_LETTER = 4
+MAX_NUM_ANY_LETTER = 6
+
 letterDict = {
     1: "A", 2: "B", 3: "C", 4: "D", 5: "E"
     , 6: "F", 7: "G", 8: "H", 9: "I", 10: "J"
@@ -87,6 +90,7 @@ def playLetterPause():
 def callsign_simpleaudio_random_3(speed):
     random.seed(None, 2)
 
+    # first letter
     rnd = random.randint(1, 4)
     # TODO - remove Alpha
     rnd = random.randint(2, 4)
@@ -116,25 +120,92 @@ def callsign_simpleaudio_random_3(speed):
     playLetterPause()
 
 
-def randomize_callsign(speed):
+def get_suffix_count():
+    return random.randint(1, 3)
+
+
+def get_prefix_count():
+    return random.randint(1, 2)
+
+
+def randomize_callsign():
     # call signs can be: 1x1, 1x2, 1x3, 2x1, 2x2, 2x3
+    result = ""
+
     random.seed(None, 2)
-    rndPrefix = random.randint(1, 2)
-    rndSuffix = random.randint(1, 3)
+    rndPrefix = get_prefix_count()
+    rndSuffix = get_suffix_count()
 
     # TODO: remove; force to be 1x3
     rndPrefix = 1
     rndSuffix = 3
 
+    # prefix letter(s)
+    if rndPrefix == 2:
+        rnd = random.randint(1, MAX_NUM_FIRST_LETTER)
+        result += getFirstLetter(rnd)
+        rnd = random.randint(1, MAX_NUM_ANY_LETTER)
+        result += getLetter(rnd)
+    else:
+        rnd = random.randint(2, MAX_NUM_FIRST_LETTER)
+        result += getFirstLetter(rnd)
+
+    # number
+    rnd = random.randint(0, 9)
+    result += getNumber(rnd)
+
+    # suffix letters
+    while rndSuffix > 1:
+        rnd = random.randint(1, MAX_NUM_ANY_LETTER)
+        result += getLetter(rnd)
+        rndSuffix = rndSuffix - 1
+    rnd = random.randint(1, MAX_NUM_ANY_LETTER)
+    result += getLetter(rnd)
+
+    return result
+
+
+def playCharacter(theChar, locn):
+    filename = locn + theChar + ".wav"
+    wave_obj = sa.WaveObject.from_wave_file(filename)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    playLetterPause()
+
+
+def play_callsign(callsign, speed):
+    size = len(callsign)
+    cnt = 1
+
+    for ele in callsign:
+        inflection = "-m"
+        if cnt == 1:
+            inflection = "-h"
+        if cnt == size:
+            inflection = "-l"
+        cnt += 1
+        playCharacter(str(ele) + inflection, speed)
+
 
 def main():
-    callsign_simpleaudio_random_3(AUDIO_SLOW)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_SLOW)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_FAST)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_FAST)
-    playPause(PAUSE_SPEED)
+    # callsign_simpleaudio_random_3(AUDIO_SLOW)
+    # playPause(PAUSE_SPEED)
+    # callsign_simpleaudio_random_3(AUDIO_SLOW)
+    # playPause(PAUSE_SPEED)
+    # callsign_simpleaudio_random_3(AUDIO_FAST)
+    # playPause(PAUSE_SPEED)
+    # callsign_simpleaudio_random_3(AUDIO_FAST)
+    # playPause(PAUSE_SPEED)
 
-# main()
+    print(randomize_callsign())
+    print(randomize_callsign())
+    callsign = randomize_callsign()
+    print(callsign)
+    for ele in callsign:
+        print(ele)
+    print("---")
+    play_callsign(callsign, AUDIO_FAST)
+    play_callsign(callsign, AUDIO_SLOW)
+
+
+main()
