703a6ab6f6980c683c09b26b72c0c92d723a4c5a
craig larsen <craig.larsen@solutiondesign.com>
2021-07-04 19:22:26 -0500

    clean up code
    * code cleanup
    * remove unused code
    * move unused audio code to new test file
    * fix tests

--- src/main/python/check-audio.py
+++ src/main/python/check-audio.py
@@ -5,29 +5,16 @@ import simpleaudio as sa
 from playsound import playsound
 import pygame
 
-def myFunction(myParam):
-    print("love my functions")
-    return "param-" + myParam
+AUDIO_LOCN_BASE = "../resource/"
+AUDIO_SPEED_FAST = "fast/"
+AUDIO_SPEED_SLOW = "mid/"
 
-def callsign_simpleaudio():
-    # using simpleaudio
-    #
-    filename = '../resource/alpha-h.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = '../resource/bravo-m.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = '../resource/eight-m.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = '../resource/november-l.wav'
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
+AUDIO_FAST = AUDIO_LOCN_BASE + AUDIO_SPEED_FAST
+AUDIO_SLOW = AUDIO_LOCN_BASE + AUDIO_SPEED_SLOW
+
+PAUSE_SPEED = 750
+PAUSE_BETW_CALLSIGNS = 0.75
+PAUSE_BETW_LETTERS = 0.10
 
     # using playsound
     # filename = '../resource/0.wav'
@@ -80,221 +67,25 @@ def callsign_simpleaudio():
 # }
 # return result
 
-letterDict = {
-    # n             n               n               n               n
-    1:"alpha",      2:"bravo",      3:"charlie",    4:"delta",      5:"echo"
-    , 6:"foxtrot",  7:"golf",       8:"hotel",      9:"india",      10:"juliett"
-    , 11:"kilo",    12:"lima",      13:"mike",      14:"november",  15:"oscar"
-    , 16:"papa",    17:"quebec",    18:"romeo",     19:"sierra",    20:"tango"
-    , 21:"uniform", 22:"victor",    23:"whiskey",   24:"x-ray",     25:"yankee"
-    , 26:"zulu"
-}
 
-tempLetterDict = {
-    1:1,            2:2,            3:3,            4:11,           5:14
-    , 6:23
-}
-firstLetterDict = {
-    "1":"1", "2":"11", "3":"14", "4":"23"
-}
-
-AUDIO_LOCN_BASE = "../resource/"
-AUDIO_SPEED_FAST = "fast/"
-AUDIO_SPEED_SLOW = "mid/"
-AUDIO_SPEED = AUDIO_SPEED_FAST
-AUDIO_LOCN = AUDIO_LOCN_BASE + AUDIO_SPEED
-AUDIO_FAST = AUDIO_LOCN_BASE + AUDIO_SPEED_FAST
-AUDIO_SLOW = AUDIO_LOCN_BASE + AUDIO_SPEED_SLOW
-PAUSE_SPEED = 750
-PAUSE_BETW_CALLSIGNS = 0.75
-PAUSE_BETW_LETTERS = 0.10
-
-def getLetter(pos):
-    result = "noletter-" + str(pos)
-    # if pos == 1:
-    #     result = "alpha"
-    # elif pos == 2:
-    #     result = "bravo"
-    # elif pos == 3:
-    #     result = "charlie"
-    # elif pos == 4:
-    #     result = "november"
-    # elif pos == 5:
-    #     result = "whiskey"
-    # return result
-
-    return letterDict[tempLetterDict[pos]]
-    # return letterDict[pos]
-
-def getFirstLetter(pos):
-    result = "noletter-" + str(pos)
-    if pos == 1:
-        result = "alpha"
-    elif pos == 2:
-        result = "kilo"
-    elif pos == 3:
-        result = "november"
-    elif pos == 4:
-        result = "whiskey"
-
-    return result
-
-def getNumber(pos):
-    # assumed pos is a number
-    return str(pos)
-
-def playFirstLetterHigh2(pos, locn):
-    filename = locn + getFirstLetter(pos) + "-h" + ".wav"
+def playLetters(pos, locn):
+    filename = locn + "alpha" + "-h" + ".wav"
     wave_obj = sa.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-
-def playFirstLetterHigh(pos):
-    playFirstLetterHigh2(pos, AUDIO_LOCN)
-
-# def playLetterHigh2(pos, locn):
-#     filename = locn + getLetter(pos) + "-h" + ".wav"
-#     wave_obj = sa.WaveObject.from_wave_file(filename)
-#     play_obj = wave_obj.play()
-#     play_obj.wait_done()
-#
-# def playLetterHigh(pos):
-#     playLetterHigh2(pos, AUDIO_LOCN)
-
-def playLetterMid2(pos, locn):
-    filename = locn + getLetter(pos) + "-m" + ".wav"
+    filename = locn + "bravo" + "-m" + ".wav"
     wave_obj = sa.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
     play_obj.wait_done()
-
-def playLetterMid(pos):
-    playLetterMid2(pos, AUDIO_LOCN)
-
-def playLetterLow2(pos, locn):
-    filename = locn + getLetter(pos) + "-l" + ".wav"
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-
-def playLetterLow(pos):
-    playLetterLow2(pos, AUDIO_LOCN)
-
-def playNumberMid2(pos, locn):
-    filename = locn + getNumber(pos) + "-m" + ".wav"
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-
-def playNumberMid(pos):
-    playNumberMid2(pos, AUDIO_LOCN)
-
-def playPause(wait):
-    time.sleep(PAUSE_BETW_CALLSIGNS)
-
-def playLetterPause():
-    time.sleep(PAUSE_BETW_LETTERS)
-
-def callsign_simpleaudio_random_1():
-    random.seed(None, 2)
-    rnd = random.randint(1, 4)
-    filename = AUDIO_LOCN + getFirstLetter(rnd) + "-h" + ".wav"
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = AUDIO_LOCN + getNumber(random.randint(0, 9)) + "-m" + ".wav"
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = AUDIO_LOCN + getLetter(random.randint(1, 5)) + "-m" + ".wav"
-    wave_obj = sa.WaveObject.from_wave_file(filename)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    filename = AUDIO_LOCN + getLetter(random.randint(1, 5)) + "-l" + ".wav"
+    filename = locn + "charlie" + "-l" + ".wav"
     wave_obj = sa.WaveObject.from_wave_file(filename)
     play_obj = wave_obj.play()
     play_obj.wait_done()
 
 
-def callsign_simpleaudio_random_2():
-    random.seed(None, 2)
-
-    rnd = random.randint(1, 4)
-    # TODO - remove Alpha
-    rnd = random.randint(2, 4)
-    playFirstLetterHigh(rnd)
-    playLetterPause()
-
-    if rnd == 1:
-        rnd = random.randint(1, 5)
-        playLetterMid(rnd)
-        playLetterPause()
-
-    rnd = random.randint(0, 9)
-    playNumberMid(rnd)
-    playLetterPause()
-
-    rndLtr = random.randint(1, 3)
-    # TODO - do a 1x3
-    rndLtr = 3
-    while rndLtr > 1:
-        rnd = random.randint(1, 5)
-        playLetterMid(rnd)
-        playLetterPause()
-        rndLtr = rndLtr - 1
-
-    rnd = random.randint(1, 5)
-    playLetterLow(rnd)
-    playLetterPause()
-
-def callsign_simpleaudio_random_3(speed):
-    random.seed(None, 2)
-
-    rnd = random.randint(1, 4)
-    # TODO - remove Alpha
-    rnd = random.randint(2, 4)
-    playFirstLetterHigh2(rnd, speed)
-    playLetterPause()
-
-    if rnd == 1:
-        rnd = random.randint(1, 5)
-        playLetterMid2(rnd, speed)
-        playLetterPause()
-
-    rnd = random.randint(0, 9)
-    playNumberMid2(rnd, speed)
-    playLetterPause()
-
-    rndLtr = random.randint(1, 3)
-    # TODO - do a 1x3
-    rndLtr = 3
-    while rndLtr > 1:
-        rnd = random.randint(1, 5)
-        playLetterMid2(rnd, speed)
-        playLetterPause()
-        rndLtr = rndLtr - 1
-
-    rnd = random.randint(1, 5)
-    playLetterLow2(rnd, speed)
-    playLetterPause()
 
 def main():
-    # callsign_simpleaudio()
-    # callsign_simpleaudio_random_1()
+    playLetters(0, AUDIO_SLOW)
 
-    # callsign_simpleaudio_random_2()
-    # playPause(PAUSE_SPEED)
-    # callsign_simpleaudio_random_2()
-    # playPause(PAUSE_SPEED)
-    # callsign_simpleaudio_random_2()
-    # playPause(PAUSE_SPEED)
-
-    callsign_simpleaudio_random_3(AUDIO_SLOW)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_SLOW)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_FAST)
-    playPause(PAUSE_SPEED)
-    callsign_simpleaudio_random_3(AUDIO_FAST)
-    playPause(PAUSE_SPEED)
 
 main()
--- src/test/python/test_audio.py
+++ src/test/python/test_audio.py
@@ -1,39 +1,46 @@
+import time
+
 from src.main.python import callsign
 
 AUDIO_LOCN_BASE = "../../main/resource/"
 
-def test_letter_1():
-    assert callsign.getLetter(1) == "alpha"
-
 def test_medium_letters_high():
+    time.sleep(0.75)
     for x in range(1,5):
         callsign.playFirstLetterHigh2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_SLOW)
 
 def test_medium_letters_mid():
-    for x in range(1,6):
+    time.sleep(0.75)
+    for x in range(1,7):
         callsign.playLetterMid2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_SLOW)
 
 def test_medium_letters_low():
-    for x in range(1,6):
+    time.sleep(0.75)
+    for x in range(1,7):
         callsign.playLetterLow2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_SLOW)
 
 def test_medium_numbers():
+    time.sleep(0.75)
     for x in range(0,10):
         callsign.playNumberMid2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_SLOW)
 
 def test_fast_letters_high():
+    time.sleep(0.75)
     for x in range(1,5):
         callsign.playFirstLetterHigh2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_FAST)
 
 def test_fast_letters_mid():
-    for x in range(1,6):
+    time.sleep(0.75)
+    for x in range(1,7):
         callsign.playLetterMid2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_FAST)
 
 def test_fast_letters_low():
-    for x in range(1,6):
+    time.sleep(0.75)
+    for x in range(1,7):
         callsign.playLetterLow2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_FAST)
 
 def test_fast_numbers():
+    time.sleep(0.75)
     for x in range(0,10):
         callsign.playNumberMid2(x, AUDIO_LOCN_BASE + callsign.AUDIO_SPEED_FAST)
 
